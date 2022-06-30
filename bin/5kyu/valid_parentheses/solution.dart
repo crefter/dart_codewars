ed0d1f3f337da0c0eb97354083a32cfa604e121c
crefter <crefter1@yandex.ru>
2022-07-01 00:34:21 +0700

    valid parentheses done

--- bin/5kyu/valid_parentheses/solution.dart
+++ bin/5kyu/valid_parentheses/solution.dart
@@ -1,3 +1,18 @@
+// 1-st version
+// bool validParentheses(String braces) {
+//   int length = 0;
+//   for (String char in braces.split("")) {
+//     char == "(" ? length++ : length--;
+//     if (length < 0) return false;
+//   }
+//
+//   return length == 0;
+// }
+
+// 2-nd version
 bool validParentheses(String braces) {
-  return false;
+  while (braces.contains("()")) {
+    braces = braces.replaceAll("()", "");
+  }
+  return braces.isEmpty ? true : false;
 }
