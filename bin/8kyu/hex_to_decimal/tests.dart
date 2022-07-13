5450446e9fd2f3c7011db650e0e9a0a3911d6d48
crefter <crefter1@yandex.ru>
2022-07-13 19:50:43 +0700

    solved "hex to decimal" task

---
+++ bin/8kyu/hex_to_decimal/tests.dart
@@ -0,0 +1,23 @@
+import "package:test/test.dart";
+
+import 'solution.dart';
+
+void main() {
+  group("Fixed tests", () {
+    test("Testing for 1", () {
+      expect(hexToDec("1"), equals(1));
+    });
+    test("Testing for a", () {
+      expect(hexToDec("a"), equals(10));
+    });
+    test("Testing for 10", () {
+      expect(hexToDec("10"), equals(16));
+    });
+    test("Testing for FF", () {
+      expect(hexToDec("FF"), equals(255));
+    });
+    test("Testing for -C", () {
+      expect(hexToDec("-C"), equals(-12));
+    });
+  });
+}
