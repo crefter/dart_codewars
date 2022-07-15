c57f4e53cf6aed257719329bfeb4a1db7f4aeb8a
Tata Rti <EvoletCir@yandex.ru>
2022-07-15 17:03:36 +0700

    solved "primes in numbers" task

---
+++ bin/5kyu/primes_in_numbers/tests.dart
@@ -0,0 +1,17 @@
+import "package:test/test.dart";
+
+import 'solution.dart';
+
+void main() {
+  dotest(int n, String exp) {
+    test("Testing for: \n$n \n", () => expect(primeFactors(n), equals(exp)));
+  }
+
+  group("fixed tests", () {
+    dotest(7775460, "(2**2)(3**3)(5)(7)(11**2)(17)");
+    dotest(7919, "(7919)");
+    dotest(17 * 17 * 93 * 677, "(3)(17**2)(31)(677)");
+    dotest(933555431, "(7537)(123863)");
+    dotest(342217392, "(2**4)(3)(11)(43)(15073)");
+  });
+}
